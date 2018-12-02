f54d574911f13bb1fc1a64338706a9855da21d6a
Java-Yoshie-Inc <44732127+Java-Yoshie-Inc@users.noreply.github.com>
2018-12-02 17:27:47 +0100

    sdfsdf

--- src/main/HandwritingRecognition.java
+++ src/main/HandwritingRecognition.java
@@ -1,9 +1,19 @@
 package main;
 
-public class HandwritingRecognition {
+import neural_network.ActivationFunctionType;
+import neural_network.NeuralNetwork;
+import neural_network.Range;
 
+public class HandwritingRecognition {
+	
+	private NeuralNetwork neuralNetwork;
+	
 	public HandwritingRecognition() {
-		
+		setupNetwork();
+	}
+	
+	private void setupNetwork() {
+		neuralNetwork = new NeuralNetwork(ActivationFunctionType.SIGMOID2, new Range(-1, 1), 0, new int[] {});
 	}
 	
 	public static void main(String[] args) {
