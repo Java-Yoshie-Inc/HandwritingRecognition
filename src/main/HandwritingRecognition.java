617bc2ab850804cd63b5187b6636cde57263fc87
JavaYoshie Inc <40612283+Yoshie2000@users.noreply.github.com>
2018-12-02 17:27:36 +0100

    Test

--- src/main/HandwritingRecognition.java
+++ src/main/HandwritingRecognition.java
@@ -1,9 +1,30 @@
 package main;
 
-public class HandwritingRecognition {
+import java.awt.Graphics;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class HandwritingRecognition {
+	
+	private JFrame frame;
+	private JPanel panel;
+	
 	public HandwritingRecognition() {
+		frame = new JFrame("Handwriting Recognition Beta");
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(3);
+		frame.setResizable(false);
 		
+		panel = new JPanel() {
+			private static final long serialVersionUID = 1L;
+			@Override
+			public void paint(Graphics g) {
+				
+			}
+		};
+		frame.add(panel);
+		frame.setVisible(true);
 	}
 	
 	public static void main(String[] args) {
